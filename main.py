83cec5221bf62eafc8e8e763d34f9f67da284d38
Arshiamalekzadeh <mkayarshia@gmail.com>
2025-07-12 22:07:14 +0330

    یشمشپ

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ api_id = 1137903
 api_hash = "6007f2a379420aa71e6ad8de2aa32c299b947017429868ca01c20fe2cf3ce5ce"
 bot_token = os.getenv("BOT_TOKEN")
 
+# به صورت مستقیم شناسه کانال را وارد کنید
+destination_channel = -1002659890273  # شناسه کانال
+
+# اگر متغیر محیطی تنظیم شده باشد، از آن استفاده کنید
 dest_channel_str = os.getenv("DEST_CHANNEL")
-if not dest_channel_str:
-    raise ValueError("Environment variable DEST_CHANNEL is not set.")
-destination_channel = int(dest_channel_str)
+if dest_channel_str:
+    destination_channel = int(dest_channel_str)
 
 app = Client("vayuo_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+
+# ارسال پیام به کانال زمانی که بات استارت می‌خورد
+@app.on_start
+async def on_start(client):
+    await client.send_message(destination_channel, "🔹 بات به طور موفقیت‌آمیز شروع شد. آماده دریافت فایل‌ها هستم.")
 
 @app.on_message(filters.private & (filters.audio | filters.video | filters.document | filters.photo))
 async def handle_file(client, message: Message):
